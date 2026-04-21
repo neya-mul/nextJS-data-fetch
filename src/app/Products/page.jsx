46bb947e65f561888385e4f51cd433b9b75bb182
neya-mul <neyamulislam946@gmail.com>
2026-04-21 19:44:15 +0600

    cache learning

--- src/app/Products/page.jsx
+++ src/app/Products/page.jsx
@@ -1,9 +1,13 @@
 import ProductCard from '@/components/ProductCard';
 import React from 'react'
+export const dynamic = 'force-dynamic';
 
 export default async function Products() {
-  const res = await fetch('http://localhost:5000/products')
+  const res = await fetch('http://localhost:5000/products', { cache: 'force-cache' })
   const products = await res.json()
+  if (!res.ok) {
+    throw new Error('Failed to fetch products');
+  }
   // console.log(products);
 
   return (
